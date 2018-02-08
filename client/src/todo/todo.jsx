e93eb1118a6203df8afd6592e9017b5bef3dcf98
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-07 23:51:46 -0300

    New todoForm and todoList components add to the app

--- client/src/todo/todo.jsx
+++ client/src/todo/todo.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+
+// Imports by dev
 import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component {
   render(){
     return (
-      <PageHeader name="Tarefas" small="Cadastro"/>
+      <div>
+        <PageHeader name="Tarefas" small="Cadastro"/>
+        <TodoForm />
+        <TodoList />
+      </div>
     )
   }
 }
