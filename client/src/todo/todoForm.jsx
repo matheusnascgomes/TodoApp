7dc88cf9913a0e5d3dc8c7a9bcc5737cfe8c9e5d
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-09 20:18:25 -0300

    New component to menage grid system bootstrap

--- client/src/todo/todoForm.jsx
+++ client/src/todo/todoForm.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Grid from '../template/grid'
 
 export default props => (
   <div role='form' className='todoForm'>
-    <div className='col-xs-12 col-sm-9 col-md-10'>
-      <input id='description' className='form-control'
-      placeholder='Adicione uma tarefa'/>
-    </div>
+
+  <Grid cols='12 9 10'>
+    <input id='description' className='form-control'
+    placeholder='Adicione uma tarefa'/>
+  </Grid>
+
+
+
+
+
+
+
+
+
+
+
+    
     <div className='col-xs-12 col-sm-3 col-md-2'>
       <button className='btn btn-primary'>
         <i className='fa fa-plus'></i>
