476e306a983bfe3ebec0d04425b44d672ebd05b7
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-09 20:40:55 -0300

    Component for button style

--- client/src/todo/todoForm.jsx
+++ client/src/todo/todoForm.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
+
 import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
   <div role='form' className='todoForm'>
 
-  <Grid cols='12 9 10'>
-    <input id='description' className='form-control'
-    placeholder='Adicione uma tarefa'/>
-  </Grid>
+    <Grid cols='12 9 10'>
+      <input id='description' className='form-control'
+      placeholder='Adicione uma tarefa'/>
+    </Grid>
 
+    <Grid cols='12 3 2'>
+      <IconButton style='primary' icon='plus' />  
+    </Grid>
 
-
-
-
-
-
-
-
-
-
-    
-    <div className='col-xs-12 col-sm-3 col-md-2'>
-      <button className='btn btn-primary'>
-        <i className='fa fa-plus'></i>
-      </button>
-    </div>
   </div>
 )
