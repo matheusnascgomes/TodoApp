ced517c1440706fa91db8342d90cb80ec04446d6
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-15 11:01:13 -0300

    Post activities

--- client/src/todo/todoForm.jsx
+++ client/src/todo/todoForm.jsx
@@ -8,11 +8,15 @@ export default props => (
 
     <Grid cols='12 9 10'>
       <input id='description' className='form-control'
+      value={props.description}
+      onChange={props.handleChange}
       placeholder='Adicione uma tarefa'/>
     </Grid>
 
     <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus' />  
+      <IconButton style='primary' icon='plus'
+        onClick={props.handleAdd}
+      />
     </Grid>
 
   </div>
