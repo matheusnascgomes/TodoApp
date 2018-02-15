bdbf1e4712facc48fd47ccb05b1095c3c982517e
Matheus Gomes <matheusnascgomes@gmail.com>
2018-02-15 11:51:02 -0300

    Insert and delete of activities

--- client/src/todo/todoList.jsx
+++ client/src/todo/todoList.jsx
@@ -1,5 +1,35 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-  <h3>Lista de atividades</h3>
-)
+export default props => {
+
+  const renderList = () => {
+    const list = props.list || []
+
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td>
+          <IconButton
+            style='danger'
+            icon='trash-o'
+            onClick={() => props.handleRemove(todo)}
+            ></IconButton>
+        </td>
+      </tr>
+    ))
+  }
+
+  return(
+    <table className="table">
+      <thead>
+        <tr>
+          <td>Descrição</td>
+        </tr>
+      </thead>
+      <tbody>
+          {renderList()}
+      </tbody>
+    </table>
+  )
+}
